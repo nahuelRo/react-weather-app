ce0bca9706d5599c000b94ea09da334ea8731c0f
unknown <nahuelr.developer@gmail.com>
2022-11-10 20:30:18 -0300

    v1

---
+++ src/api.js
@@ -0,0 +1,13 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "2b7feec810msh50356dc5e8929edp1314e7jsn9ffcc894550c",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+
+export const WEATHER_API_KEY = "769cc79a4f9095edfcd9c1db6fb16e2d";
